2d2eae0fec81cda229511b17d1f912e17101fbd3
Rus <tudy.rusy@gmail.com>
2020-04-30 22:08:42 +0300

    final version

---
+++ TaskManager1/TaskManager1/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace TaskManager1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Processes = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.end = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.nameC = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.idc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cpuc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Performance = new System.Windows.Forms.TabPage();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Info = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ramC = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1.SuspendLayout();
+            this.Processes.SuspendLayout();
+            this.Performance.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.Info.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Processes);
+            this.tabControl1.Controls.Add(this.Performance);
+            this.tabControl1.Controls.Add(this.Info);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(638, 548);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Processes
+            // 
+            this.Processes.Controls.Add(this.button1);
+            this.Processes.Controls.Add(this.textBox1);
+            this.Processes.Controls.Add(this.start);
+            this.Processes.Controls.Add(this.end);
+            this.Processes.Controls.Add(this.listView1);
+            this.Processes.Location = new System.Drawing.Point(4, 25);
+            this.Processes.Name = "Processes";
+            this.Processes.Padding = new System.Windows.Forms.Padding(3);
+            this.Processes.Size = new System.Drawing.Size(630, 519);
+            this.Processes.TabIndex = 0;
+            this.Processes.Text = "Proceses";
+            this.Processes.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(97, 376);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
+            // 
+            // start
+            // 
+            this.start.Enabled = false;
+            this.start.Location = new System.Drawing.Point(7, 375);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(83, 33);
+            this.start.TabIndex = 2;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Visible = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // end
+            // 
+            this.end.Location = new System.Drawing.Point(444, 375);
+            this.end.Name = "end";
+            this.end.Size = new System.Drawing.Size(107, 33);
+            this.end.TabIndex = 1;
+            this.end.Text = "End Task";
+            this.end.UseVisualStyleBackColor = true;
+            this.end.Click += new System.EventHandler(this.end_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameC,
+            this.idc,
+            this.ramC,
+            this.cpuc});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 6);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(545, 363);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // nameC
+            // 
+            this.nameC.Text = "Name";
+            this.nameC.Width = 155;
+            // 
+            // idc
+            // 
+            this.idc.Text = "ID";
+            this.idc.Width = 75;
+            // 
+            // cpuc
+            // 
+            this.cpuc.Text = "CPU";
+            this.cpuc.Width = 66;
+            // 
+            // Performance
+            // 
+            this.Performance.Controls.Add(this.chart2);
+            this.Performance.Controls.Add(this.chart1);
+            this.Performance.Location = new System.Drawing.Point(4, 25);
+            this.Performance.Name = "Performance";
+            this.Performance.Padding = new System.Windows.Forms.Padding(3);
+            this.Performance.Size = new System.Drawing.Size(630, 519);
+            this.Performance.TabIndex = 1;
+            this.Performance.Text = "Performance";
+            this.Performance.UseVisualStyleBackColor = true;
+            // 
+            // chart2
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(6, 250);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Lime;
+            series1.Legend = "Legend1";
+            series1.Name = "RAM%";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(618, 263);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(6, 6);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Blue;
+            series2.Legend = "Legend1";
+            series2.Name = "CPU%";
+            series2.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(618, 238);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Info
+            // 
+            this.Info.Controls.Add(this.label1);
+            this.Info.Location = new System.Drawing.Point(4, 25);
+            this.Info.Name = "Info";
+            this.Info.Padding = new System.Windows.Forms.Padding(3);
+            this.Info.Size = new System.Drawing.Size(630, 519);
+            this.Info.TabIndex = 2;
+            this.Info.Text = "Info";
+            this.Info.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // ramC
+            // 
+            this.ramC.Text = "RAM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 572);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "TaskManager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.Processes.ResumeLayout(false);
+            this.Processes.PerformLayout();
+            this.Performance.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.Info.ResumeLayout(false);
+            this.Info.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Processes;
+        private System.Windows.Forms.Button end;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TabPage Performance;
+        private System.Windows.Forms.TabPage Info;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.ColumnHeader nameC;
+        private System.Windows.Forms.ColumnHeader idc;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader cpuc;
+        private System.Windows.Forms.ColumnHeader ramC;
+    }
+}
+
---
+++ TaskManager1/TaskManager1/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Threading;
+using System.Management;
+using Timer = System.Windows.Forms.Timer;
+
+namespace TaskManager1
+{
+    
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        Process[] processList;
+        private Thread cpuThread;
+        private Thread pThread;
+        private Thread ramThread;
+        private double[] ramArray = new double[60];
+        private double[] cpuArray = new double[60];
+        
+        string info;
+        string s1;
+        float s;
+        PerformanceCounter C = new PerformanceCounter();
+        
+        private void getPerfCounters()
+        {
+            var cpuPerfCounter = new PerformanceCounter("Processor Information","% Processor Time","_Total");
+
+            while (true)
+            {
+                cpuArray[cpuArray.Length - 1] = Math.Round(cpuPerfCounter.NextValue(), 0);
+                Array.Copy(cpuArray, 1, cpuArray, 0, cpuArray.Length - 1);
+
+                if (chart1.IsHandleCreated)
+                    this.Invoke((MethodInvoker)delegate { UpdateCpuChart(); });
+                Thread.Sleep(500);
+            }
+        }
+        private void getPerfCounters2()
+        {
+            var ramPerfCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+
+            while (true)
+            {
+                ramArray[ramArray.Length - 1] = Math.Round(ramPerfCounter.NextValue(), 0);
+                Array.Copy(ramArray, 1, ramArray, 0, ramArray.Length - 1);
+
+                if (chart2.IsHandleCreated)
+                    this.Invoke((MethodInvoker)delegate { UpdateRamChart(); });
+                Thread.Sleep(500);
+            }
+        }
+        private void UpdateCpuChart()
+        {
+            chart1.Series["CPU%"].Points.Clear();
+
+            for (int i = 0; i < cpuArray.Length - 1; ++i)
+            {
+                chart1.Series["CPU%"].Points.AddY(cpuArray[i]);
+            }
+        }
+        private void UpdateRamChart()
+        {
+            chart2.Series["RAM%"].Points.Clear();
+
+            for (int i = 0; i < ramArray.Length - 1; ++i)
+            {
+                chart2.Series["RAM%"].Points.AddY(ramArray[i]-8);
+            }
+        }
+        public void loadProcessList()
+        {
+            listView1.Items.Clear();
+            processList = Process.GetProcesses();
+            /*var counters = new List<PerformanceCounter>();
+            foreach (Process p in processList)
+            {
+                var counter = new PerformanceCounter("Process", "% Processor Time", p.ProcessName);
+                counter.NextValue();
+                counters.Add(counter);
+            }
+            Thread.Sleep(100);
+            int i = 0;*/
+            foreach (Process p in processList)
+            {
+                try
+                {
+
+
+                    //s1 = counters[i++].NextValue().ToString();
+                    
+                    ListViewItem item = new ListViewItem(new[] { p.ProcessName, p.Id.ToString(),(p.WorkingSet64/1000).ToString()+" k","0"});
+                    item.Tag = p;
+                    //Console.WriteLine(s1);
+                    listView1.Items.Add(item);
+
+                }
+                catch { }
+                
+                //item.SubItems.Add(p.Id.ToString());
+            }
+          
+        }
+        public void updateProcessList()
+        {
+            //listView1.Items.Clear();
+
+            /*var counters = new List<PerformanceCounter>();
+            foreach (Process p in processList)
+            {
+                var counter = new PerformanceCounter("Process", "% Processor Time", p.ProcessName);
+                counter.NextValue();
+                counters.Add(counter);
+            }
+            Thread.Sleep(100);
+            int i = 0;*/
+            Process[] pnew = Process.GetProcesses();
+            int i = 0;
+            foreach (ListViewItem item in listView1.Items)
+            {
+                try
+                {
+                    //s1 = counters[i++].NextValue().ToString();
+                    Process p = Process.GetProcessById(int.Parse(item.SubItems[1].Text));
+                    item.SubItems[2].Text = (p.WorkingSet64 / 1000).ToString() + " k" ;
+                    //Console.WriteLine("da "+i++);
+                }
+                catch { }
+
+                //item.SubItems.Add(p.Id.ToString());
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            string text = start.Text;
+            Process.Start(text);
+            loadProcessList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //listView1.View = View.Details;
+            loadProcessList();
+
+            cpuThread = new Thread(new ThreadStart(this.getPerfCounters));
+            cpuThread.IsBackground = true;
+            cpuThread.Start();
+
+            ramThread = new Thread(new ThreadStart(this.getPerfCounters2));
+            ramThread.IsBackground = true;
+            ramThread.Start();
+            //info
+            
+
+            //os and cpu
+            var wmi =
+            new ManagementObjectSearcher("select * from Win32_OperatingSystem")
+            .Get()
+            .Cast<ManagementObject>()
+            .First();
+
+            var cpu =
+            new ManagementObjectSearcher("select * from Win32_Processor")
+            .Get()
+            .Cast<ManagementObject>()
+            .First();
+
+            info += "OS Name:           " + ((string)wmi["Caption"]).Trim() + "\n" +
+                    "OS Version:        " + (string)wmi["Version"] + "\n" +
+                    "OS Architecture:   " + (string)wmi["OSArchitecture"] + "\n" +
+                    "\n\n" +
+                    "CPU Name:          " + ((string)cpu["Name"]).Replace("(TM)", "™")
+                                                                   .Replace("(tm)", "™")
+                                                                   .Replace("(R)", "®")
+                                                                   .Replace("(r)", "®")
+                                                                   .Replace("(C)", "©")
+                                                                   .Replace("(c)", "©")
+                                                                   .Replace("    ", " ")
+                                                                   .Replace("  ", " ") + "\n"+
+                    "CPU ID:            " + (string)cpu["ProcessorId"]+"\n"+
+                    "CPU Socket:        " + (string)cpu["SocketDesignation"]+"\n"+
+                    "CPU SpeedMHz:      " + ((uint)cpu["MaxClockSpeed"]).ToString() + "\n" +
+                    "CPU BusSpeedMHz:   " + ((uint)cpu["ExtClock"]).ToString() + "\n" +
+                    "CPU Cores:         " + ((uint)cpu["NumberOfCores"]).ToString() + "\n" +
+                    "CPU Threads:       " + ((uint)cpu["NumberOfLogicalProcessors"]).ToString();
+            //mem
+            ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(wql);
+            ManagementObjectCollection results = searcher.Get();
+
+            foreach (ManagementObject result in results)
+            {
+                info += "\n\n\nTotal Visible Memory:   " + result["TotalVisibleMemorySize"] +" KB\n";
+                info += "Free Physical Memory: "+ result["FreePhysicalMemory"] + " KB\n";
+                info += "Total Virtual Memory:   "+ result["TotalVirtualMemorySize"] + " KB\n";
+                info += "Free Virtual Memory:    "+ result["FreeVirtualMemory"] + " KB\n";
+            }
+            
+            C.CategoryName = "Process";
+            C.CounterName = "% Processor Time";
+            C.InstanceName = "";
+            C.ReadOnly = true;
+            label1.Text = info;
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = (2000) * (1);
+            timer.Enabled = true;
+            timer.Start();
+            chart2.ChartAreas[0].AxisY.Maximum = 100;
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            updateProcessList();
+        }
+        private void end_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            Process p = (Process)item.Tag;
+            p.Kill();
+            loadProcessList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updateProcessList();
+        }
+    }
+}
